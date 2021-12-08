c046f265c70e3e9223dca31108d90425d01dd20e
Kostiantyn Verenych <kverenych@atlassian.com>
2021-12-08 22:51:56 +0200

    reactgraphql: Add state with useEffect hook and pass there a user name

--- reactgraphql/src/App.js
+++ reactgraphql/src/App.js
@@ -1,7 +1,9 @@
 import github from './db'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
+  let [userName, setUserName] = useState('')
+
   useEffect(() => {
     const githubQuery = {
       query: `
@@ -20,18 +22,20 @@ function App() {
     })
       .then((response) => response.json())
       .then((data) => {
+        setUserName(data.data.viewer.name)
         console.log(data)
       })
       .catch((err) => {
         console.log(err)
       })
-  })
+  }, [])
 
   return (
     <div className="container mt-5">
       <h1 className="text-primary">
         <i className="bi bi-diagram-2-fill"></i> Repos
       </h1>
+      <p>Hey there {userName}</p>
     </div>
   )
 }
