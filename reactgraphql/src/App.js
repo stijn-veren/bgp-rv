04d46a99a6487af26c9e64beb5e142968eb01e5a
Kostiantyn Verenych <kverenych@atlassian.com>
2021-12-09 12:47:30 +0200

    reactgraphql: Add generating a list of repos

--- reactgraphql/src/App.js
+++ reactgraphql/src/App.js
@@ -1,20 +1,22 @@
-import github from './db'
+import github from './db.js'
 import { useEffect, useState, useCallback } from 'react'
-import githubQuery from './Query'
+import query from './Query'
 
 function App() {
   let [userName, setUserName] = useState('')
+  let [repoList, setRepoList] = useState(null)
 
   const fetchData = useCallback(() => {
     fetch(github.baseURL, {
       method: 'POST',
       headers: github.headers,
-      body: JSON.stringify(githubQuery),
+      body: JSON.stringify(query),
     })
       .then((response) => response.json())
       .then((data) => {
-        setUserName(data.data.viewer.name)
-        console.log(data)
+        const viewer = data.data.viewer
+        setUserName(viewer.name)
+        setRepoList(viewer.repositories.nodes)
       })
       .catch((err) => {
         console.log(err)
@@ -26,11 +28,24 @@ function App() {
   }, [fetchData])
 
   return (
-    <div className="container mt-5">
+    <div className="App container mt-5">
       <h1 className="text-primary">
         <i className="bi bi-diagram-2-fill"></i> Repos
       </h1>
       <p>Hey there {userName}</p>
+
+      {repoList && (
+        <ul className="list-group list-group-flush">
+          {repoList.map((repo) => (
+            <li className="list-group-item" key={repo.id.toString()}>
+              <a className="h5 mb-0 text-decoration-none" href={repo.url}>
+                {repo.name}
+              </a>
+              <p className="small">{repo.description}</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
